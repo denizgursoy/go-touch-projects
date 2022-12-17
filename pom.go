da016690c4168e6b916eb5073d8aab4732357a26
denizgursoy <denizgursoy.443@gmail.com>
2022-12-17 14:16:48 +0100

    [#4] add template

--- pom.go
+++ pom.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"html/template"
 	"os"
+	"text/template"
 )
 
+type input struct {
+	fileName string
+	values   map[string]interface{}
+}
+
 func main() {
-
-	file, err2 := os.ReadFile("medium-pom.xml")
-	if err2 != nil {
-		return
-	}
-	tmpl, err := template.New("test").Parse(string(file))
-	if err != nil {
-		panic(err)
-	}
-
-	m := map[string]interface{}{
+	inputs := make([]input, 0)
+	mediumValues := map[string]interface{}{
 		"Dependencies": []interface{}{
 			map[string]string{
 				"groupId":    "org.postgresql",
@@ -25,8 +21,44 @@ func main() {
 			},
 		},
 	}
-	err = tmpl.Execute(os.Stdout, m)
-	if err != nil {
-		panic(err)
+	githubPages := map[string]interface{}{
+		"endpoints": []map[string]string{
+			{
+				"method": "GET",
+				"path":   "/",
+				"status": "http.StatusOK",
+				"body":   "Hello, World!",
+			},
+			{
+				"method": "POST",
+				"path":   "/payment",
+				"status": "http.StatusOK",
+				"body":   "Payment is created",
+			},
+		},
+	}
+	mediumWebServerValues := map[string]interface{}{
+		"httpLibrary": "gin",
+	}
+	inputs = append(inputs,
+		input{fileName: "medium-pom.xml", values: mediumValues},
+		input{fileName: "github-pages.txt", values: githubPages},
+		input{fileName: "x.txt", values: mediumWebServerValues},
+	)
+
+	for _, i := range inputs {
+		file, err2 := os.ReadFile(i.fileName)
+		if err2 != nil {
+			return
+		}
+		tmpl, err := template.New("test").Parse(string(file))
+		if err != nil {
+			panic(err)
+		}
+
+		err = tmpl.Execute(os.Stdout, i.values)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
